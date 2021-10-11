661ef9d280c97265e82e95731a5cd98d4e1d6083
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-11 13:14:27 +0100

    closes resource

--- src/main/java/uk/ac/ebi/pepvep/utils/Codon2AminoAcid.java
+++ src/main/java/uk/ac/ebi/pepvep/utils/Codon2AminoAcid.java
@@ -1,6 +1,10 @@
 package uk.ac.ebi.pepvep.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -10,6 +14,7 @@ import java.util.Map;
 //Combine three nucleotide (from gene) makes one codon
 //Left hand codon right hand amino acid in 3 letters
 public class Codon2AminoAcid {
+  private static final Logger logger = LoggerFactory.getLogger(Codon2AminoAcid.class);
 	private static final String CODON_MAPPING_FILE = "codon.txt";
 	private static Map<String, String> codonAminoAcidMap = null;
 
@@ -17,16 +22,19 @@ public class Codon2AminoAcid {
 		codonAminoAcidMap = new HashMap<>();
 		InputStream inputFS = Codon2AminoAcid.class.getClassLoader().getResourceAsStream(CODON_MAPPING_FILE);
 		assert inputFS != null;
-		BufferedReader br = new BufferedReader(new InputStreamReader(inputFS, StandardCharsets.UTF_8));
-		br.lines()
-			.filter(Commons::notNullNotEmpty)
-			.map(Commons::trim)
-			.filter(line -> !line.startsWith("#"))
-			.forEach(line -> {
-			String[] p = line.split("=");
-			codonAminoAcidMap.put(p[0], p[1]);
-		});
-	}
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputFS, StandardCharsets.UTF_8))) {
+      br.lines()
+        .filter(Commons::notNullNotEmpty)
+        .map(Commons::trim)
+        .filter(line -> !line.startsWith("#"))
+        .forEach(line -> {
+          String[] p = line.split("=");
+          codonAminoAcidMap.put(p[0], p[1]);
+        });
+    } catch (IOException e) {
+      logger.error("Failed to close file", e);
+    }
+  }
 
 	public static String getAminoAcid(String codon) {
 		if (codonAminoAcidMap == null)
