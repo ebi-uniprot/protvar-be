6c4fdfceae8e9c5c143dc1e599f348b70ac4b3cb
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 07:59:04 +0100

    separates pwd for two env

--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -1,3 +1,3 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
-  --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$db_pwd \
+  --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
   --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --dry-run=client -o yaml | kubectl apply -f -
