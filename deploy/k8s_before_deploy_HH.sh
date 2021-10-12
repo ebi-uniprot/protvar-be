85fbc8b6954999edb5c1f81afd589d716304127a
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 13:05:53 +0100

    uses system wide proxy instead of JVM

--- deploy/k8s_before_deploy_HH.sh
+++ deploy/k8s_before_deploy_HH.sh
@@ -1,7 +1,5 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hh_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hh_db_pwd \
-  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' --from-literal=http.proxySet=true \
-  --from-literal=http.proxyHost=hh-wwwcache.ebi.ac.uk --from-literal=http.proxyPort=3128 \
-  --from-literal=https.proxyHost=hh-wwwcache.ebi.ac.uk --from-literal=https.proxyPort=3128 \
-  --from-literal=http.nonProxyHosts='localhost|127.0.0.1|.cluster.local' --from-literal=https.proxySet=true \
+  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' --from-literal=no_proxy=localhost,.cluster.local,127.0.0.1 \
+  --from-literal=http_proxy=http://hh-wwwcache.ebi.ac.uk:3128 --from-literal=https_proxy=http://hh-wwwcache.ebi.ac.uk:3128  \
   --dry-run=client -o yaml | kubectl apply -f -
--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -1,7 +1,5 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
-  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=http.proxySet=true \
-  --from-literal=http.proxyHost=hx-wwwcache.ebi.ac.uk --from-literal=http.proxyPort=3128 \
-  --from-literal=https.proxyHost=hx-wwwcache.ebi.ac.uk --from-literal=https.proxyPort=3128 \
-  --from-literal=http.nonProxyHosts='localhost|127.0.0.1|.cluster.local' --from-literal=https.proxySet=true \
+  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=no_proxy=localhost,.cluster.local,127.0.0.1 \
+  --from-literal=http_proxy=http://hx-wwwcache.ebi.ac.uk:3128 --from-literal=https_proxy=http://hx-wwwcache.ebi.ac.uk:3128  \
   --dry-run=client -o yaml | kubectl apply -f -
