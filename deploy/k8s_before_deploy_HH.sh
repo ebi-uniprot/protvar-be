10d3f3f0d09c46c71d258cd46768ad53398a6b53
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 14:05:56 +0100

    corrects smtp host

--- deploy/k8s_before_deploy_HH.sh
+++ deploy/k8s_before_deploy_HH.sh
@@ -3,5 +3,5 @@ jvm+=" -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttps.pr
 jvm+=" -Dhttp.nonProxyHosts=localhost|127.0.0.1|.cluster.local"
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hh_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hh_db_pwd \
-  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
+  --from-literal=spring.mail.host='hh-smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
   --dry-run=client -o yaml | kubectl apply -f -
--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -3,5 +3,5 @@ jvm+=" -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttps.pr
 jvm+=" -Dhttp.nonProxyHosts=localhost|127.0.0.1|.cluster.local"
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
-  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
+  --from-literal=spring.mail.host='hx-smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
   --dry-run=client -o yaml | kubectl apply -f -
