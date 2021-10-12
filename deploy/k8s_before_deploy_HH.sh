adf4e170a4a15af145e0002350b629fdd251781f
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 13:27:32 +0100

    uses env variable for jvm
    https://docs.oracle.com/javase/8/docs/technotes/guides/troubleshoot/envvars002.html

--- deploy/k8s_before_deploy_HH.sh
+++ deploy/k8s_before_deploy_HH.sh
@@ -1,5 +1,5 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hh_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hh_db_pwd \
-  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' --from-literal=no_proxy=localhost,.cluster.local,127.0.0.1 \
-  --from-literal=http_proxy=http://hh-wwwcache.ebi.ac.uk:3128 --from-literal=https_proxy=http://hh-wwwcache.ebi.ac.uk:3128  \
+  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' \
+  --from-literal=JAVA_TOOL_OPTIONS=" -Dhttp.proxySet=true -Dhttp.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttps.proxySet=true -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttp.nonProxyHosts='localhost|127.0.0.1|.cluster.local' " \
   --dry-run=client -o yaml | kubectl apply -f -
--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -1,5 +1,5 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
-  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=no_proxy=localhost,.cluster.local,127.0.0.1 \
-  --from-literal=http_proxy=http://hx-wwwcache.ebi.ac.uk:3128 --from-literal=https_proxy=http://hx-wwwcache.ebi.ac.uk:3128  \
+  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' \
+  --from-literal=JAVA_TOOL_OPTIONS=" -Dhttp.proxySet=true -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttps.proxySet=true -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttp.nonProxyHosts='localhost|127.0.0.1|.cluster.local' " \
   --dry-run=client -o yaml | kubectl apply -f -
