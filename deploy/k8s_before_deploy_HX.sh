6a22ee7de90c7bc04cfc4ae0d9e9ca8dbe81ab7d
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 12:07:55 +0100

    adds proxies

--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -1,3 +1,7 @@
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
-  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --dry-run=client -o yaml | kubectl apply -f -
+  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=http.proxySet=true \
+  --from-literal=http.proxyHost=hx-wwwcache.ebi.ac.uk --from-literal=http.proxyPort=3128 \
+  --from-literal=https.proxyHost=hx-wwwcache.ebi.ac.uk --from-literal=https.proxyPort=3128 \
+  --from-literal=http.nonProxyHosts='localhost|127.0.0.1|.cluster.local' --from-literal=https.proxySet=true \
+  --dry-run=client -o yaml | kubectl apply -f -
