3505680e74074c98238b9c61357899cbbb367a9a
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-10-12 13:41:23 +0100

    tidy up JVM options

--- deploy/k8s_before_deploy_HH.sh
+++ deploy/k8s_before_deploy_HH.sh
@@ -1,5 +1,7 @@
+jvm="-Dhttp.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttp.proxySet=true"
+jvm+=" -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttps.proxySet=true"
+jvm+=" -Dhttp.nonProxyHosts=localhost|127.0.0.1|.cluster.local"
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hh_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hh_db_pwd \
-  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' \
-  --from-literal=JAVA_TOOL_OPTIONS=" -Dhttp.proxySet=true -Dhttp.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttps.proxySet=true -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttp.nonProxyHosts='localhost|127.0.0.1|.cluster.local' " \
+  --from-literal=spring.mail.host='hh.smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
   --dry-run=client -o yaml | kubectl apply -f -
--- deploy/k8s_before_deploy_HX.sh
+++ deploy/k8s_before_deploy_HX.sh
@@ -1,5 +1,7 @@
+jvm="-Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttp.proxySet=true"
+jvm+=" -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttps.proxySet=true"
+jvm+=" -Dhttp.nonProxyHosts=localhost|127.0.0.1|.cluster.local"
 kubectl create secret generic properties --from-literal=spring.datasource.url=$hx_db_url \
   --from-literal=spring.datasource.username=$db_user --from-literal=spring.datasource.password=$hx_db_pwd \
-  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' \
-  --from-literal=JAVA_TOOL_OPTIONS=" -Dhttp.proxySet=true -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttps.proxySet=true -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk -Dhttps.proxyPort=3128 -Dhttp.nonProxyHosts='localhost|127.0.0.1|.cluster.local' " \
+  --from-literal=spring.mail.host='hx.smtp.ebi.ac.uk' --from-literal=JAVA_TOOL_OPTIONS="${jvm}" \
   --dry-run=client -o yaml | kubectl apply -f -
