7625e72af72e784faca9fd1845c1c6ca1324c595
Prabhat Totoo <prabhat@ebi.ac.uk>
2022-06-22 16:53:10 +0100

    new constant values

--- src/main/java/uk/ac/ebi/protvar/utils/Constants.java
+++ src/main/java/uk/ac/ebi/protvar/utils/Constants.java
@@ -17,4 +17,7 @@ public class Constants {
 	public static final String NOTE_INVALID_INPUT_ALT = "Invalid alternative ";
 	public static final String NOTE_INVALID_INPUT_REF = "Invalid reference ";
 	public static final String NOTE_INVALID_INPUT_ACCESSION = "Invalid accession ";
+	public static final String NOTE_INVALID_INPUT_PROTEIN_POSITION = "Invalid protein position ";
+	public static final String NOTE_INVALID_INPUT_AA = "Invalid reference or alternative amino acid ";
+	public static final String NOTE_INVALID_INPUT_NON_SNV = "Non SNV ";
 }
