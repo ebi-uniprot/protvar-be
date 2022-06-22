3f45fb2a6c56444faa334f0aa22bd2cdd04b1530
Prabhat Totoo <prabhat@ebi.ac.uk>
2022-06-22 17:03:29 +0100

    fixed existing tests

--- src/test/java/uk/ac/ebi/protvar/model/UserInputVCFParserTest.java
+++ src/test/java/uk/ac/ebi/protvar/model/UserInputVCFParserTest.java
@@ -6,33 +6,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import uk.ac.ebi.protvar.parser.GenericParser;
+import uk.ac.ebi.protvar.parser.VCFParser;
 import uk.ac.ebi.protvar.utils.Constants;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static uk.ac.ebi.protvar.model.UserInput.DB_MT_CHROMOSOME;
+import static uk.ac.ebi.protvar.parser.GenericParser.DB_MT_CHROMOSOME;
 
 class UserInputVCFParserTest {
   @ParameterizedTest
   @NullAndEmptySource
   void emptyLineIsInvalidObject(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
-    assertEquals(UserInput.invalidObject(input), userInput);
+    UserInput userInput = VCFParser.parse(input);
+    assertEquals(UserInput.invalidObject(input, UserInput.Type.VCF), userInput);
   }
 
   @Test
   void minimumInput() {
     var input = "x 123 a/t";
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     assertInput(true, input, "X", 123L, Constants.NA, "A", "T", "X-123", userInput);
   }
 
   @Test
   void inputWithoutId() {
     var input = "mt 4 g c";
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     assertInput(true, input, DB_MT_CHROMOSOME, 4L, Constants.NA, "G", "C", DB_MT_CHROMOSOME + "-4", userInput);
   }
 
@@ -40,14 +42,14 @@ class UserInputVCFParserTest {
   @ValueSource(strings = {"y 987654321 rs123 t g", "y 987654321 rs123 t g description;",
     "y 987654321 rs123 t g 100 PASS AA=T|||;AC=1;AF=0"})
   void inputWithId(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     assertInput(true, input, "Y", 987654321L, "rs123", "T", "G", "Y-987654321", userInput);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"1", "2", "X", "y", "21"})
   void onlyChromProvided(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     assertInput(false, input, input.toUpperCase(), -1L, Constants.NA, Constants.NA, Constants.NA, input.toUpperCase() + "--1", userInput);
     assertEquals("Position should be a number |Invalid reference |Invalid alternative ", userInput.getInvalidReason());
   }
@@ -55,7 +57,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @ValueSource(strings = {"3 123", "4 456", "x 789", "Y 987", "22 654"})
   void chromAndPositionProvided(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     String[] tokens = input.split(" ");
     assertInput(false, input, tokens[0].toUpperCase(), Long.parseLong(tokens[1]), Constants.NA, Constants.NA, Constants.NA,
       tokens[0].toUpperCase() + "-" + tokens[1], userInput);
@@ -65,7 +67,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @ValueSource(strings = {"5 123 1", "6 456 .", "7 789 rc12", "8 987 n/a"})
   void threeInputs_3rdWillConsiderId(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     String[] tokens = input.split(" ");
     assertInput(false, input, tokens[0].toUpperCase(), Long.parseLong(tokens[1]), tokens[2], Constants.NA, Constants.NA,
       tokens[0].toUpperCase() + "-" + tokens[1], userInput);
@@ -75,7 +77,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @ValueSource(strings = {"5 123 A", "6 456 C", "7 789 G", "8 987 T"})
   void threeInputs_3rdWillNotConsiderIdIfItIsAllele(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     String[] tokens = input.split(" ");
     assertInput(false, input, tokens[0].toUpperCase(), Long.parseLong(tokens[1]), Constants.NA, tokens[2], Constants.NA,
       tokens[0].toUpperCase() + "-" + tokens[1], userInput);
@@ -85,7 +87,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @ValueSource(strings = {"9 123 . a", "10 456 c1 c", "11 789 1g g", "12 987 45 t"})
   void chromPositionIdRefProvided(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     String[] tokens = input.split(" ");
     assertInput(false, input, tokens[0].toUpperCase(), Long.parseLong(tokens[1]), tokens[2], tokens[3].toUpperCase(), Constants.NA,
       tokens[0].toUpperCase() + "-" + tokens[1], userInput);
@@ -95,7 +97,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @CsvSource({"23 1,23,1", "z 2,z,2", "mat 3,mat,3"})
   void invalidChromValidPosition(String input, String chr, Long pos) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     assertInput(false, input, Constants.NA, pos, Constants.NA, Constants.NA, Constants.NA, "N/A-" + pos, userInput);
     assertEquals("Invalid chromosome "+chr+"|Invalid reference |Invalid alternative ", userInput.getInvalidReason());
   }
@@ -103,7 +105,7 @@ class UserInputVCFParserTest {
   @ParameterizedTest
   @ValueSource(strings = {"13 0", "14 a", "15 $", "16 a1", "17 b2£", "18 *t"})
   void validChromInvalidPosition(String input) {
-    UserInput userInput = UserInput.VCFParser.parse(input);
+    UserInput userInput = VCFParser.parse(input);
     String[] tokens = input.split(" ");
     assertInput(false, input, tokens[0], -1L, Constants.NA, Constants.NA, Constants.NA, tokens[0]+"--1", userInput);
     assertEquals("Position should be a number "+tokens[1]+"|Invalid reference |Invalid alternative ", userInput.getInvalidReason());
@@ -126,7 +128,7 @@ class UserInputVCFParserTest {
   private void assertInput(boolean valid, String input, String chr, Long pos, String id, String ref, String alt,
                            String group, UserInput actual) {
     assertEquals(valid, actual.isValid());
-    assertEquals(input, actual.getInputString());
+    assertEquals(input, actual.getFormattedInputString());
     assertEquals(chr, actual.getChromosome());
     assertEquals(pos, actual.getStart());
     assertEquals(id, actual.getId());
@@ -139,25 +141,25 @@ class UserInputVCFParserTest {
   class isIdTest {
     @Test
     void emptyListWillSayCannotGetUserId() {
-      assertFalse(UserInput.VCFParser.isIdExist(new LinkedList<>()));
+      assertFalse(VCFParser.isIdExist(new LinkedList<>()));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"A", "C", "G", "T"})
     void alleleCannotBeUserIds(String allele) {
-      assertFalse(UserInput.VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
+      assertFalse(VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"a", "c", "g", "t"})
     void smallCaseAlleleCannotBeUserIds(String allele) {
-      assertFalse(UserInput.VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
+      assertFalse(VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"A/C", "C/G", "G/T", "T/A", "c/a", "g/c", "t/g", "a/t"})
     void combinationAlleleCannotBeUserIds(String allele) {
-      assertFalse(UserInput.VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
+      assertFalse(VCFParser.isIdExist(new LinkedList<>(List.of(allele))));
     }
   }
 }
