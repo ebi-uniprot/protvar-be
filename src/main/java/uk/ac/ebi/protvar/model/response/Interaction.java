5ea77e5a236e595f4b7bd80372ceaa9882cfc4a3
prabhat <prabhat@ebi.ac.uk>
2023-01-25 12:05:33 +0000

    merged into single interaction entity

--- src/main/java/uk/ac/ebi/protvar/model/response/Interaction.java
+++ src/main/java/uk/ac/ebi/protvar/model/response/Interaction.java
@@ -4,19 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Interaction {
     private String a;
+    private List<Integer> aresidues;
     private String b;
+    private List<Integer> bresidues;
     private Double pdockq;
-    private String modelUrl;
+    private String pdbModel;
 
-    public Interaction(String a, String b, Double pdockq) {
+    public Interaction(String a, List<Integer> aresidues, String b, List<Integer> bresidues, Double pdockq) {
         this.a = a;
+        this.aresidues = aresidues;
         this.b = b;
+        this.bresidues = bresidues;
         this.pdockq = pdockq;
-        this.modelUrl = "/interaction/{a}/{b}/model".replace("{a}", a)
+        this.pdbModel = "/interaction/{a}/{b}/model".replace("{a}", a)
                 .replace("{b}", b);
     }
 }
