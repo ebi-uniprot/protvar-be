413d0fd02426168f4a7488b0b24c8b10f07602c5
prabhat <prabhat@ebi.ac.uk>
2023-04-05 22:03:54 +0100

    New mapping response with list of user inputs (and corresponding mappings) and messages

--- src/main/java/uk/ac/ebi/protvar/model/response/MappingResponse.java
+++ src/main/java/uk/ac/ebi/protvar/model/response/MappingResponse.java
@@ -1,30 +1,13 @@
 package uk.ac.ebi.protvar.model.response;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import uk.ac.ebi.protvar.input.UserInput;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class MappingResponse {
-	//List<GenomeProteinMapping> mappings = new ArrayList<>();
-	//List<UserInput> invalidInputs = new ArrayList<>();
-
-	String userInput;
-	List<GenomeProteinMapping> mappings = new ArrayList<>();
-
-	//Map<String, List<GenomeProteinMapping>> mappings = new LinkedHashMap<>(); // K=input, V=list of mappings for input
-
-	//List<GenomeProteinMapping> toList() {
-	//	return mappings.values()
-	//			.stream()
-	//			.flatMap(Collection::stream)
-	//			.collect(Collectors.toList());
-	//}
-
+	List<UserInput> inputs;
+	List<Message> messages;
 }
