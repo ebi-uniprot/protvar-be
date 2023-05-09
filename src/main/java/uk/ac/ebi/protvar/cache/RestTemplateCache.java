d508e8fd5c37d2a77c544fc78d94d230c672ff84
prabhat <prabhat@ebi.ac.uk>
2023-05-09 09:39:44 +0100

    made restTemplateCache thread-safe

--- src/main/java/uk/ac/ebi/protvar/cache/RestTemplateCache.java
+++ src/main/java/uk/ac/ebi/protvar/cache/RestTemplateCache.java
@@ -1,12 +1,12 @@
 package uk.ac.ebi.protvar.cache;
 
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,43 +14,31 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nonnull;
 
 public class RestTemplateCache extends RestTemplate {
-  private final int SIZE = 3;
+  //private final int MAX_CACHE_SIZE = 10000;
   private static final Logger logger = LoggerFactory.getLogger(RestTemplateCache.class);
-  private final List<Data> cache = new ArrayList<>(SIZE);
+
+  // RestTemplate level cache (L1) to determine an external API call is needed or not
+  // Cached data is response entity
+  private final Map<URI, ResponseEntity<?>> cache = new ConcurrentHashMap<>();
 
   @Nonnull
   @Override
   public <T> ResponseEntity<T> getForEntity(@Nonnull URI url, @Nonnull Class<T> responseType) throws RestClientException {
     var response = getFromCache(url);
     if (Objects.nonNull(response)) {
-      logger.trace("Using Cache to Response back for {}", url);
+      logger.info("Using RestTemplate cache for {}", url);
       return (ResponseEntity<T>) response;
     }
 
     var apiResponse = super.getForEntity(url, responseType);
-    updateCache(url, apiResponse);
+    cache.put(url, apiResponse);
+
     return apiResponse;
   }
 
-  private void updateCache(@Nonnull URI url, @Nonnull ResponseEntity<?> response) {
-    if (cache.size() >= SIZE) {
-      cache.remove(0);
-    }
-    cache.add(new Data(url, response));
-  }
-
-  @AllArgsConstructor
-  private static class Data {
-    private URI key;
-    private ResponseEntity<?> value;
-  }
-
   private ResponseEntity<?> getFromCache(URI key) {
-    for (int i = cache.size() - 1; i >= 0; i--) {
-      var ele = cache.get(i);
-      if (ele.key.equals(key))
-        return ele.value;
-    }
+    if (cache.containsKey(key))
+      return cache.get(key);
     return null;
   }
 }
