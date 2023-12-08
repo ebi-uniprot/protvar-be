668758f5344f12e1636f414e8411815e91cf30db
prabhat <prabhat@ebi.ac.uk>
2023-12-08 10:38:58 +0000

    added rsAcc property for HGVS protein input

--- src/main/java/uk/ac/ebi/protvar/input/format/protein/HGVSp.java
+++ src/main/java/uk/ac/ebi/protvar/input/format/protein/HGVSp.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.protvar.input.format.protein;
 
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.protvar.exception.InvalidInputException;
@@ -21,6 +22,7 @@ import java.util.regex.Pattern;
  * NP_xxx:p.Trp78*       -> AA3 + POS + *
  * NP_xxx:p.W87*         -> AA1 + POS + AA1
  */
+@Getter
 public class HGVSp extends ProteinInput {
     private static final Logger LOGGER = LoggerFactory.getLogger(HGVSp.class);
     public static final String PREFIX = RefSeqUtils.NP; // -> lookup transcript (enst)?
@@ -37,6 +39,9 @@ public class HGVSp extends ProteinInput {
 
     private static Pattern patternOneLetter = Pattern.compile(REGEX + ONE_LETTER_AA_SUB, Pattern.CASE_INSENSITIVE);
     private static Pattern patternThreeLetter = Pattern.compile(REGEX + THREE_LETTER_AA_SUB, Pattern.CASE_INSENSITIVE);
+
+
+    String rsAcc;
 
     private HGVSp(String inputStr) {
         super(inputStr);
@@ -74,7 +79,7 @@ public class HGVSp extends ProteinInput {
         String pos = matcher.group("pos");
         String ref = matcher.group("ref");
         String alt = matcher.group("alt");
-        this.acc = rsAcc;
+        this.rsAcc = rsAcc;
         this.pos = Integer.parseInt(pos);
         this.ref = ref;
         this.alt = alt;
