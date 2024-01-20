d23da165d378fd3682c666e26fa4df64c6bb7176
prabhat <prabhat@ebi.ac.uk>
2024-01-20 09:38:56 +0000

    moved logic in new InputProcessor class

--- src/main/java/uk/ac/ebi/protvar/input/UserInput.java
+++ src/main/java/uk/ac/ebi/protvar/input/UserInput.java
@@ -1,22 +1,16 @@
 package uk.ac.ebi.protvar.input;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import uk.ac.ebi.protvar.input.format.coding.HGVSc;
-import uk.ac.ebi.protvar.input.format.genomic.Gnomad;
-import uk.ac.ebi.protvar.input.format.genomic.HGVSg;
 import uk.ac.ebi.protvar.input.format.id.ClinVarID;
 import uk.ac.ebi.protvar.input.format.id.CosmicID;
-import uk.ac.ebi.protvar.input.format.id.DbsnpID;
-import uk.ac.ebi.protvar.input.format.genomic.VCF;
-import uk.ac.ebi.protvar.input.format.protein.HGVSp;
 import uk.ac.ebi.protvar.input.type.GenomicInput;
-import uk.ac.ebi.protvar.input.type.ProteinInput;
 import uk.ac.ebi.protvar.model.response.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *                            UserInput
@@ -68,70 +62,6 @@ public abstract class UserInput {
 		return messages.stream().filter(m ->  m.getType() == Message.MessageType.ERROR).map(m -> m.getText()).collect(Collectors.toList());
 	}
 
-
-	/**
-	 * Parse input string into a UserInput object.
-	 * @param inputStr	Null and empty inputs will have been filtered before calling this function.
-	 *             Input string will have been trimmed as well.
-	 * @return
-	 */
-	public static UserInput getInput(String inputStr) {
-		if (inputStr == null || inputStr.isEmpty())
-			return null;
-
-		// Steps:
-		// LEVEL 1 (first-level) CHECK
-		// Only check if input meets "general" pattern for a specific type for e.g. if input starts with a certain
-		// prefix, for e.g. XX, we assume it is of a specific type.
-		// This happens at the top-level "if" condition below, before using the appropriate parser for full parsing.
-		// LEVEL 2 (second-level) CHECK
-		// Full parsing of input string to extract all attributes happens in L2.
-		// Here we may find that we are dealing with a specific input type, but it doesn't full parse (or contain all
-		// the info we require)
-
-		// ID/ref checks - these should be single-word input
-		if (DbsnpID.startsWithPrefix(inputStr)) {
-			return DbsnpID.parse(inputStr);
-		}
-		else if (ClinVarID.startsWithPrefix(inputStr)) {
-			return ClinVarID.parse(inputStr);
-		}
-		else if (CosmicID.startsWithPrefix(inputStr)) {
-			return CosmicID.parse(inputStr);
-		}
-		// HGVS checks - should also be single-word input
-		else if (HGVSg.maybeHGVSg(inputStr)) { //  just because check on startsWithPrefix isn't enough
-			return HGVSg.parse(inputStr);
-		}
-		else if (HGVSc.maybeHGVSc(inputStr)) {
-			return HGVSc.parse(inputStr);
-		}
-		else if (HGVSp.maybeHGVSp(inputStr)) {
-			return HGVSp.parse(inputStr);
-		}
-		// GNOMAD ID check - again this is a single-word ('-' separated) input
-		else if (Gnomad.isValid(inputStr)) {
-			return Gnomad.parse(inputStr);
-		}
-		// Remaining input formats at this point
-		// - custom protein -> always starts with an accession
-		// - VCF and generic/custom genomic -> always starts with a chr
-		else if (ProteinInput.startsWithAccession(inputStr)) {
-			return ProteinInput.parse(inputStr);
-		}
-		else if (GenomicInput.startsWithChromo(inputStr)) {
-			// VCF if input sticks to strict format, otherwise, maybe custom
-			if (VCF.isValid(inputStr)) {
-				return VCF.parse(inputStr);
-			}
-			else if (GenomicInput.isValid(inputStr)) {
-				return GenomicInput.parse(inputStr);
-			}
-		}
-		return GenomicInput.invalidInput(inputStr); // default (or most common) input is expected to be genomic, so
-												// let's assume any invalid input is of GenomicInput type.
-	}
-
 	@JsonIgnore
 	public String getClinVarIDPrefix() {
 		if (this instanceof ClinVarID
@@ -163,4 +93,6 @@ public abstract class UserInput {
 		}
 		return chrPosList;
 	}
+
+
 }
