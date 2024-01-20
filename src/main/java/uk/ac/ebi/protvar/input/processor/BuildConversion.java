10f77fa13639df591be654f7063a1dd7a7f56da3
prabhat <prabhat@ebi.ac.uk>
2024-01-20 09:25:11 +0000

    renamed and moved main logic from MappingFetcher

--- src/main/java/uk/ac/ebi/protvar/input/processor/BuildConversion.java
+++ src/main/java/uk/ac/ebi/protvar/input/processor/BuildConversion.java
@@ -2,10 +2,12 @@ package uk.ac.ebi.protvar.input.processor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.protvar.input.Type;
 import uk.ac.ebi.protvar.input.UserInput;
 import uk.ac.ebi.protvar.input.type.GenomicInput;
 import uk.ac.ebi.protvar.model.data.Crossmap;
 import uk.ac.ebi.protvar.model.grc.Assembly;
+import uk.ac.ebi.protvar.model.response.MappingResponse;
 import uk.ac.ebi.protvar.model.response.Message;
 import uk.ac.ebi.protvar.repo.ProtVarDataRepo;
 
@@ -15,10 +17,61 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class BuildConverter {
+public class BuildConversion {
 
     @Autowired
     private ProtVarDataRepo protVarDataRepo;
+
+    public void process(Map<Type, List<UserInput>> groupedInputs, String assemblyVersion, MappingResponse response) {
+        List<Message> messages = response.getMessages();
+        if (groupedInputs.containsKey(Type.GENOMIC)) {
+            List<UserInput> genomicInputs = groupedInputs.get(Type.GENOMIC);
+            boolean allGenomic = groupedInputs.size() == 1;
+
+            /**   assembly
+             *   |    |    \
+             *   v    v     \
+             *  null auto    v
+             *   |    |     37or38
+             *   | detect   |    |  \
+             *   | |   \    |    |  /undetermined(assumes)
+             *    \ \   v   v    v  v
+             *     \ \  is37 ..> is38
+             *      \_\___________^
+             *
+             *        ..> converts to
+             */
+
+            // null | auto | 37or38
+            Assembly assembly = null;
+
+            if (assemblyVersion == null) {
+                messages.add(new Message(Message.MessageType.WARN, "Unspecified assembly version; defaulting to GRCh38"));
+                assembly = Assembly.GRCH38;
+            } else {
+                if (assemblyVersion.equalsIgnoreCase("AUTO")) {
+                    if (allGenomic) {
+                        assembly = determineBuild(genomicInputs, messages); // -> 37 or 38
+                    }
+                    else {
+                        messages.add(new Message(Message.MessageType.WARN, "Assembly auto-detect works for all-genomic inputs only; defaulting to GRCh38"));
+                        assembly = Assembly.GRCH38;
+                    }
+                } else {
+                    assembly = Assembly.of(assemblyVersion);
+                    if (assembly == null) {
+                        messages.add(new Message(Message.MessageType.WARN, "Unable to determine assembly version; defaulting to GRCh38"));
+                        assembly = Assembly.GRCH38;
+                    }
+                }
+            }
+
+            if (assembly == Assembly.GRCH37) {
+                messages.add(new Message(Message.MessageType.INFO, "Converting GRCh37 to GRCh38"));
+                convert(genomicInputs);
+            }
+        }
+    }
 
     /**
      * Process genomic inputs
@@ -28,7 +81,7 @@ public class BuildConverter {
      * - the latter is tackled in the main mapping logic.
      * @param genomicInputs
      */
-    public void convert(List<UserInput> genomicInputs) {
+    private void convert(List<UserInput> genomicInputs) {
 
         List<Object[]> chrPos37 = new ArrayList<>();
         genomicInputs.stream().map(i -> (GenomicInput) i).forEach(input -> {
@@ -57,7 +110,7 @@ public class BuildConverter {
     // select distinct chr,grch38_pos,grch38_base from crossmap where (chr,grch38_pos,grch38_base) IN (('X',149498202,'C'),
     //('10',43118436,'A'),
     //('2',233760498,'G'))
-    public Assembly determineBuild(List<UserInput> genomicInputs, List<Message> messages) {
+    private Assembly determineBuild(List<UserInput> genomicInputs, List<Message> messages) {
 
         List<Object[]> chrPosRefList = new ArrayList<>();
         genomicInputs.stream().map(i -> (GenomicInput) i).forEach(input -> {
