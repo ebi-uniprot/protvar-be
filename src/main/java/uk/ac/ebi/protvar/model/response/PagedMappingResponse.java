8f981d913e579a875b403abd15790a48cbbcd108
prabhat <prabhat@ebi.ac.uk>
2024-01-20 09:34:00 +0000

    new page mapping response for mapping retrieval by accession

---
+++ src/main/java/uk/ac/ebi/protvar/model/response/PagedMappingResponse.java
@@ -0,0 +1,22 @@
+package uk.ac.ebi.protvar.model.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagedMappingResponse {
+	private MappingResponse content;
+
+	private int pageNo;
+
+	private int pageSize;
+
+	private long totalElements;
+
+	private int totalPages;
+
+	private boolean last;
+}
