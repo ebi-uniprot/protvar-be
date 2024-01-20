fa84cc2de58d6c821f0d47e65363d5518fc89fcb
prabhat <prabhat@ebi.ac.uk>
2024-01-20 09:42:59 +0000

    redis service config

---
+++ src/main/java/uk/ac/ebi/protvar/RedisConfig.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.protvar;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${REDIS_HOST:localhost}")
+    private String redisHostString;
+    @Value("${REDIS_PORT:6379}")
+    private String redisPortString;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
