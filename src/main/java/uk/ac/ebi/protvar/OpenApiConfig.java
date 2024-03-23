af47eb0be94e2aabd13665832ae60a7b90373dde
prabhat <prabhat@ebi.ac.uk>
2024-03-23 15:16:17 +0000

    added contact us to api doc

--- src/main/java/uk/ac/ebi/protvar/OpenApiConfig.java
+++ src/main/java/uk/ac/ebi/protvar/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.protvar;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -14,6 +15,9 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .externalDocs(new ExternalDocumentation()
+                        .description("Contact us").url("mailto:protvar@ebi.ac.uk")
+                )
                 .info(new Info()
                         .title("ProtVar API")
                         .contact(new Contact().name("ProtVar").url("https://www.ebi.ac.uk/ProtVar/")));
