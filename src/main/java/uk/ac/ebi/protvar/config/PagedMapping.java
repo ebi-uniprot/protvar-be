be271e680241057babce7c1ff7fd790fe5e21eb0
prabhat <prabhat@ebi.ac.uk>
2024-06-12 13:58:49 +0100

    paged mapping settings/defaults

---
+++ src/main/java/uk/ac/ebi/protvar/config/PagedMapping.java
@@ -0,0 +1,10 @@
+package uk.ac.ebi.protvar.config;
+
+public class PagedMapping {
+
+    public final static String PAGE = "1";
+    public final static String PAGE_SIZE = "25";
+    public final static int DEFAULT_PAGE = Integer.valueOf(PAGE);
+    public final static int DEFAULT_PAGE_SIZE = Integer.valueOf(PAGE_SIZE);
+    public static int RESULT_EXPIRES_AFTER_DAYS = 30;
+}
