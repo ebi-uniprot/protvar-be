1da6769e8ff939593f7ae4297b2017aab7d61555
prabhat <prabhat@ebi.ac.uk>
2024-06-18 22:08:42 +0100

    fixed failing test, dep missing

--- src/test/java/uk/ac/ebi/protvar/fetcher/csv/CSVDataFetcherTest.java
+++ src/test/java/uk/ac/ebi/protvar/fetcher/csv/CSVDataFetcherTest.java
@@ -4,6 +4,7 @@ import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.redis.core.RedisTemplate;
 import uk.ac.ebi.protvar.input.processor.InputProcessor;
 import uk.ac.ebi.protvar.service.DownloadService;
 import uk.ac.ebi.protvar.utils.Constants;
@@ -17,7 +18,8 @@ class CSVDataFetcherTest {
 
   private static final int TOTAL_CSV_COLUMNS = 43;
   CSVDataFetcher mockDeps = new CSVDataFetcher(mock(MappingFetcher.class), mock(CSVFunctionDataFetcher.class),
-    mock(CSVPopulationDataFetcher.class), mock(CSVStructureDataFetcher.class), mock(InputProcessor.class), "");
+    mock(CSVPopulationDataFetcher.class), mock(CSVStructureDataFetcher.class), mock(InputProcessor.class), "",
+          mock(RedisTemplate.class));
 
   @Nested
   class Header {
