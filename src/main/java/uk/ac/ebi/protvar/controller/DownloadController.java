dc7b252de9fda2e641f0021b08f5e0df5528ce07
prabhat <prabhat@ebi.ac.uk>
2024-07-16 16:17:48 +0100

    reviewed the first post download endpoints, use cache in all, create filename and use it instead of id

--- src/main/java/uk/ac/ebi/protvar/controller/DownloadController.java
+++ src/main/java/uk/ac/ebi/protvar/controller/DownloadController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,15 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.ac.ebi.protvar.cache.InputCache;
+import uk.ac.ebi.protvar.config.PagedMapping;
 import uk.ac.ebi.protvar.model.DownloadRequest;
 import uk.ac.ebi.protvar.model.response.DownloadResponse;
 import uk.ac.ebi.protvar.model.response.DownloadStatus;
 import uk.ac.ebi.protvar.service.DownloadService;
-import uk.ac.ebi.protvar.utils.FileUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileInputStream;
-import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -29,9 +29,16 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 @AllArgsConstructor
+/**
+ * Download request using:
+ * 1. file input            -> returns all
+ * 2. string (text) inputs  -> returns all
+ * 3. id input              -> returns specified page num (and page size)
+ *
+ */
 public class DownloadController implements WebMvcConfigurer {
 
-  @Autowired
+  private InputCache inputCache;
   private DownloadService downloadService;
 
   /**
@@ -45,7 +52,7 @@ public class DownloadController implements WebMvcConfigurer {
    * @param function
    * @param population
    * @param structure
-   * @return response has the job ID to check status
+   * @return response contains an ID to check status
    * @throws Exception
    */
   @Operation(summary = "Submit download request for the file input and provided parameters")
@@ -58,11 +65,16 @@ public class DownloadController implements WebMvcConfigurer {
                                     @RequestParam(required = false) String assembly,
                                     @RequestParam(required = false) String email,
                                     @RequestParam(required = false) String jobName) throws Exception {
-    // TODO: cache input?
-    Path newFile = FileUtils.writeFile(downloadService.tmpPath(), file);
-    DownloadRequest downloadRequest = DownloadRequest.fileDownloadRequest(request.getRequestURL().toString(),
-            newFile, function, population, structure,
+    String id = inputCache.cacheFileInput(file);
+    DownloadRequest downloadRequest = newDownloadRequest(id, function, population, structure,
             assembly, email, jobName);
+
+    // <id>[-fun][-pop][-str][-ASSEMBLY]
+    String filename = getFilename(id, function, population, structure, null, null, assembly);
+    downloadRequest.setFname(filename);
+    String url = request.getRequestURL().toString().replace("fileInput", filename);
+    downloadRequest.setUrl(url);
+
     DownloadResponse response = downloadService.queueRequest(downloadRequest);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
@@ -76,7 +88,7 @@ public class DownloadController implements WebMvcConfigurer {
    * @param function
    * @param population
    * @param structure
-   * @return response has the job ID to check status
+   * @return response contains an ID to check status
    */
   @Operation(summary = "Submit download request for the list of inputs and provided parameters")
   @PostMapping(value = "/download/textInput", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -88,23 +100,30 @@ public class DownloadController implements WebMvcConfigurer {
           @RequestParam(required = false) String assembly,
           @RequestParam(required = false) String email,
           @RequestParam(required = false) String jobName) {
-    // TODO: cache input?
-    DownloadRequest downloadRequest = DownloadRequest.textDownloadRequest(request.getRequestURL().toString(),
-            inputs, function, population, structure,
+    String id = inputCache.cacheTextInput(String.join(System.lineSeparator(), inputs));
+
+    DownloadRequest downloadRequest = newDownloadRequest(id, function, population, structure,
             assembly, email, jobName);
+
+    // <id>[-fun][-pop][-str][-ASSEMBLY]
+    String filename = getFilename(id, function, population, structure, null, null, assembly);
+    downloadRequest.setFname(filename);
+    String url = request.getRequestURL().toString().replace("textInput", filename);
+    downloadRequest.setUrl(url);
+
     DownloadResponse response = downloadService.queueRequest(downloadRequest);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
   @Operation(summary = "Submit download request for the input ID and provided parameters including page and pageSize. " +
           "If no page is specified, the full original input is processed.")
-  @PostMapping(value = "/download/idInput", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/download/id", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   public ResponseEntity<?> download(HttpServletRequest request,
           @io.swagger.v3.oas.annotations.parameters.RequestBody(
                   description = "The unique ID of the input to generate download for."
           )
-          @RequestBody String inputId,
+          @RequestBody String id,
           @Parameter(description = "The page number to retrieve. If not specified, download file is generated for all inputs.")
           @RequestParam(required = false) Integer page,
           @Parameter(description = "The number of results per page.")
@@ -115,33 +134,87 @@ public class DownloadController implements WebMvcConfigurer {
           @RequestParam(required = false) String assembly,
           @RequestParam(required = false) String email,
           @RequestParam(required = false) String jobName) {
-    DownloadRequest downloadRequest = DownloadRequest.idDownloadRequest(request.getRequestURL().toString(),
-            inputId, page, pageSize,
-            function, population, structure,
+    DownloadRequest downloadRequest = newDownloadRequest(id, function, population, structure,
             assembly, email, jobName);
+
+    downloadRequest.setPage(page);
+    downloadRequest.setPageSize(pageSize);
+    // <id>[-fun][-pop][-str][-PAGE][-PAGE_SIZE][-ASSEMBLY]
+    String filename = getFilename(id, function, population, structure, page, pageSize, assembly);
+    downloadRequest.setFname(filename);
+    String url = request.getRequestURL().toString().replace("id", filename);
+    downloadRequest.setUrl(url);
     DownloadResponse response = downloadService.queueRequest(downloadRequest);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
+  private String getFilename(String id,
+                             boolean function, boolean population, boolean structure,
+                             Integer page, Integer pageSize, String assembly) {
+
+    String filename = id; // <id>[OPTIONS]
+
+    if (function)
+      filename += "-fun";
+    if (population)
+      filename += "-pop";
+    if (structure)
+      filename += "-str";
+
+    if (page == null && pageSize == null) {
+      // page options not provided, assume default
+      // do not append page/pageSize to filename (return ALL input mappings)
+    } else if ((page != null && page == PagedMapping.DEFAULT_PAGE) &&
+            (pageSize != null && pageSize == PagedMapping.DEFAULT_PAGE_SIZE)) {
+      // default page options
+      // do not append page/pageSize to filename (return ALL input mappings)
+    } else {
+      filename += (page == null ? "" : "-" + page);
+      filename += (pageSize == null ? "" : "-" + pageSize);
+    }
+
+    if (assembly == null || assembly.equals("AUTO")) {
+      // default assembly
+      // do not append assembly
+    } else {
+      filename += "-" + assembly;
+    }
+    return filename;
+  }
+
+  private DownloadRequest newDownloadRequest(String id, boolean function, boolean population, boolean structure,
+                                                 String assembly, String email, String jobName) {
+    DownloadRequest downloadRequest = new DownloadRequest();
+    downloadRequest.setTimestamp(LocalDateTime.now());
+    downloadRequest.setId(id);
+    downloadRequest.setFunction(function);
+    downloadRequest.setPopulation(population);
+    downloadRequest.setStructure(structure);
+    downloadRequest.setAssembly(assembly);
+    downloadRequest.setEmail(email);
+    downloadRequest.setJobName(jobName);
+    return downloadRequest;
+  }
+
   /**
    * Download results as CSV file.
-   * @param id
+   * @param filename <id>[-fun][-pop][-str][-PAGE][-PAGE_SIZE][-ASSEMBLY]
    * @return
    */
   @Operation(summary = "Download results file")
-  @GetMapping(value = "/download/{id}")
+  @GetMapping(value = "/download/{filename}")
   @ResponseBody
   public ResponseEntity<?> downloadFile(
-          @Parameter(example = "cc3b5e1a21fd") @PathVariable("id") String id) {
+          @Parameter(example = "cc3b5e1a21fd") @PathVariable("filename") String filename) {
 
-    FileInputStream fileInputStream = downloadService.getFileResource(id);
+    FileInputStream fileInputStream = downloadService.getFileResource(filename);
     if (fileInputStream == null)
       return new ResponseEntity<>("File not found", HttpStatus.NOT_FOUND);
 
     InputStreamResource resource = new InputStreamResource(fileInputStream);
 
       String contentType = "application/zip";
-      String headerValue = "attachment; filename=" + id + ".csv.zip";
+      String headerValue = "attachment; filename=" + filename + ".csv.zip";
 
       return ResponseEntity.ok()
               .contentType(MediaType.parseMediaType(contentType))
@@ -151,13 +224,14 @@ public class DownloadController implements WebMvcConfigurer {
 
   /**
    * Check download status.
-   * @param ids List of job/download IDs
+   * @param fs List of download files. The file name follows the pattern:
+   *           <id>[-PAGE][-PAGE_SIZE][-ASSEMBLY]
    * @return
    */
   @Operation(summary = "Check status of a list of download requests")
   @PostMapping(value = "/download/status", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Map<String, DownloadStatus>> downloadStatus(@RequestBody List<String> ids) {
-    return new ResponseEntity<>(downloadService.getDownloadStatus(ids), HttpStatus.OK);
+  public ResponseEntity<Map<String, DownloadStatus>> downloadStatus(@RequestBody List<String> fs) {
+    return new ResponseEntity<>(downloadService.getDownloadStatus(fs), HttpStatus.OK);
   }
 
 }
