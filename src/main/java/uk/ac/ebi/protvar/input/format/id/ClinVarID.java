15cabf877376b2358daa25735f0742fd309f6381
prabhat <prabhat@ebi.ac.uk>
2024-08-02 12:56:07 +0100

    clinvar prefix starts with no space and now case insensitive

--- src/main/java/uk/ac/ebi/protvar/input/format/id/ClinVarID.java
+++ src/main/java/uk/ac/ebi/protvar/input/format/id/ClinVarID.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import uk.ac.ebi.protvar.input.ErrorConstants;
 import uk.ac.ebi.protvar.input.Format;
 import uk.ac.ebi.protvar.input.type.IDInput;
-import uk.ac.ebi.protvar.utils.RegexUtils;
 
 import java.util.regex.Pattern;
 
@@ -18,40 +17,39 @@ import java.util.regex.Pattern;
 public class ClinVarID extends IDInput {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClinVarID.class);
+    public static final int PREFIX_LEN = 3;
     public static final String RCV = "RCV";
     public static final String VCV = "VCV";
 
-
-    public static final int PREFIX_LEN = 3;
-    public static final String PREFIX = String.format("(%s|%s)", RCV, VCV);
-    public static final String REGEX = PREFIX + "(\\d{9})(\\.\\d+)?";
+    static final String PREFIX = String.format("^(%s|%s)", RCV, VCV);
+    static final Pattern PREFIX_PATTERN = Pattern.compile(PREFIX, Pattern.CASE_INSENSITIVE);
+    static final Pattern PATTERN = Pattern.compile(PREFIX + "\\d{9}(\\.\\d+)?", Pattern.CASE_INSENSITIVE);
 
     public ClinVarID(String inputStr) {
         super(inputStr);
         setFormat(Format.CLINVAR);
+        setId(inputStr.toUpperCase()); // for db case-insensitive query
     }
 
     public static boolean startsWithPrefix(String input) {
-        return Pattern.matches("^"+PREFIX+"(.*)$", input.toUpperCase());
+        return PREFIX_PATTERN.matcher(input).find(); // Check if the string starts with any of the prefixes (case-insensitive)
     }
 
     public static ClinVarID parse(String input) {
         ClinVarID parsedInput = new ClinVarID(input);
-        if (!isValid(input)) {
+        if (!valid(input)) {
             LOGGER.warn(parsedInput + ": parsing error");
             parsedInput.addError(ErrorConstants.CLINVAR_ID_INVALID);
         }
         return parsedInput;
     }
 
-    public static boolean isValid(String input) {
-        return RegexUtils.matchIgnoreCase(REGEX, input);
+    public static boolean valid(String input) {
+        return PATTERN.matcher(input).matches();
     }
 
     @Override
     public String toString() {
         return String.format("ClinVarID [id=%s]", getId());
     }
-
-
 }
