ae31fd0df84e12463c8cfc97417260530cbdde89
prabhat <prabhat@ebi.ac.uk>
2024-09-26 09:17:50 +0100

    use new InputParams and MappingFetcher instead of MappingService, change to endpoint for singe input query

--- src/main/java/uk/ac/ebi/protvar/controller/CoordinateMappingController.java
+++ src/main/java/uk/ac/ebi/protvar/controller/CoordinateMappingController.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uk.ac.ebi.protvar.fetcher.MappingFetcher;
+import uk.ac.ebi.protvar.input.params.InputParams;
+import uk.ac.ebi.protvar.input.processor.InputProcessor;
 import uk.ac.ebi.protvar.model.response.MappingResponse;
 import uk.ac.ebi.protvar.model.response.Message;
-import uk.ac.ebi.protvar.service.MappingService;
 
 import java.util.List;
 
@@ -23,7 +25,7 @@ import java.util.List;
 public class CoordinateMappingController {
   public final static int MAX_INPUT = 1000;
   public final static String ASSEMBLY_DESC = "Specify the human genome assembly version. Accepted values are: GRCh38/h38/38, GRCh37/h37/37 or AUTO (default, auto-detects the version).";
-  private MappingService mappingService;
+  private MappingFetcher mappingFetcher;
 
   @Operation(
           summary = "Genomic-Protein Mappings for Various Input Formats",
@@ -54,7 +56,17 @@ public class CoordinateMappingController {
     if (inputs.size() > MAX_INPUT) {
       processList = inputs.subList(0, MAX_INPUT);
     }
-    MappingResponse mappingResponse = mappingService.getMapping(processList, function, population, structure, assembly);
+
+    InputParams params = InputParams.builder()
+            .inputs(InputProcessor.parse(processList))
+            .fun(function)
+            .pop(population)
+            .str(structure)
+            .assembly(assembly)
+            .summarise(true)
+            .build();
+
+    MappingResponse mappingResponse = mappingFetcher.getMapping(params);
     if (inputs.size() > MAX_INPUT && mappingResponse != null && mappingResponse.getMessages() != null) {
       mappingResponse.getMessages().add(new Message(Message.MessageType.WARN, String.format("Processed first %d inputs only.", MAX_INPUT)));
     }
@@ -63,14 +75,18 @@ public class CoordinateMappingController {
 
   @Operation(
           summary = "Retrieve Mapping for a Single Input Query",
-          description = "Fetch the genomic-protein mapping for a single input query provided as a path parameter in the URL. " +
-                  "The input should be in a supported format, such as genomic coordinates, variant identifiers, or protein change notations. "
+          description = "Fetch the genomic-protein mapping for a single input query provided as search parameter in the URL. " +
+                  "The input should be in a supported format, such as genomic coordinates, variant identifiers, or protein change notations. " +
+                  "Default input params: annotations false, assembly null, and summarise false."
   )
-  @GetMapping(value = "/mapping/query/{input}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<MappingResponse> queryInput(
+  @GetMapping(value = "/mapping", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<MappingResponse> mapping(
           @Parameter(description = "The input query in a supported format.", example = "19-1010539-G-C")
-          @PathVariable("input") String input) {
-    MappingResponse mappingResponse = mappingService.getMapping(List.of(input), false, false, false, null);
+          @RequestParam String input) {
+      InputParams params = InputParams.builder()
+              .inputs(InputProcessor.parse(List.of(input)))
+              .build();
+    MappingResponse mappingResponse = mappingFetcher.getMapping(params);
     return new ResponseEntity<>(mappingResponse, HttpStatus.OK);
   }
 
