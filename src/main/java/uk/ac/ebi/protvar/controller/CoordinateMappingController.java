d830f1a5d703b9551db540ae022f48d8355b2f3b
prabhat <prabhat@ebi.ac.uk>
2024-10-12 14:28:15 +0100

    Post-fetch: add input summary to response

--- src/main/java/uk/ac/ebi/protvar/controller/CoordinateMappingController.java
+++ src/main/java/uk/ac/ebi/protvar/controller/CoordinateMappingController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.ebi.protvar.fetcher.MappingFetcher;
+import uk.ac.ebi.protvar.input.UserInput;
 import uk.ac.ebi.protvar.input.params.InputParams;
 import uk.ac.ebi.protvar.input.processor.InputProcessor;
 import uk.ac.ebi.protvar.model.response.MappingResponse;
@@ -23,71 +24,84 @@ import java.util.List;
 @CrossOrigin
 @AllArgsConstructor
 public class CoordinateMappingController {
-  public final static int MAX_INPUT = 1000;
-  public final static String ASSEMBLY_DESC = "Specify the human genome assembly version. Accepted values are: GRCh38/h38/38, GRCh37/h37/37 or AUTO (default, auto-detects the version).";
-  private MappingFetcher mappingFetcher;
+    public final static int MAX_INPUT = 1000;
+    public final static String ASSEMBLY_DESC = "Specify the human genome assembly version. Accepted values are: GRCh38/h38/38, GRCh37/h37/37 or AUTO (default, auto-detects the version).";
+    private MappingFetcher mappingFetcher;
 
-  @Operation(
-          summary = "Genomic-Protein Mappings for Various Input Formats",
-          description = "Retrieve genomic-protein mappings for a list of inputs in any supported format. These formats include " +
-                  "genomic (VCF, gnomAD, HGVSg, and various custom formats), protein (HGVSp and various custom formats), " +
-                  "cDNA (HGVSc), and variant ID (dbSNP, ClinVar, COSMIC). UPDATE: This endpoint processes up to 1000 inputs per request now. " +
-                  "If the input size exceeds this limit, only the first 1000 inputs will be processed and returned. For larger inputs, " +
-                  "please use the new mapping paginated endpoint, which returns a paginated response."
-  )
-  @PostMapping(value = "/mappings", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<MappingResponse> mappings(
-          @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                  content = @Content(
-                          examples = @ExampleObject(value = "[\"19 1010539 G C\",\"P80404 Gln56Arg\", \"rs1042779\"]")
-                  )
-          )
-        @RequestBody List<String> inputs,
-        @Parameter(description = "Include functional annotations in results")
-        @RequestParam(required = false, defaultValue = "false") boolean function,
-        @Parameter(description = "Include population annotations (residue co-located variants and disease associations) in results")
-        @RequestParam(required = false, defaultValue = "false") boolean population,
-        @Parameter(description = "Include structural annotations in results")
-        @RequestParam(required = false, defaultValue = "false") boolean structure,
-        @Parameter(description = ASSEMBLY_DESC)
-        @RequestParam(required = false) String assembly
-  ) {
-    List<String> processList = inputs;
-    if (inputs.size() > MAX_INPUT) {
-      processList = inputs.subList(0, MAX_INPUT);
+    @Operation(
+            summary = "Legacy genomic-to-protein mapping endpoint.",
+            description = "Retrieve genomic-protein mappings for the given inputs list. Refer to the [ProtVar Help](https://www.ebi.ac.uk/ProtVar/help#supported-format) for supported input formats. " +
+                    "<br/>**Note: This endpoint is deprecated and will be removed in a future version.**" +
+                    "<br/>**Note: This endpoint is limited to up to 1000 inputs per request.**" +
+                    "<br/>Please use the paginated mapping endpoint instead."
+    )
+    @PostMapping(value = "/mappings", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MappingResponse> mappings(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            examples = @ExampleObject(value = "[\"19 1010539 G C\",\"P80404 Gln56Arg\", \"rs1042779\"]")
+                    )
+            )
+            @RequestBody List<String> inputs,
+            @Parameter(description = "Include functional annotations in results")
+            @RequestParam(required = false, defaultValue = "false") boolean function,
+            @Parameter(description = "Include population annotations (residue co-located variants and disease associations) in results")
+            @RequestParam(required = false, defaultValue = "false") boolean population,
+            @Parameter(description = "Include structural annotations in results")
+            @RequestParam(required = false, defaultValue = "false") boolean structure,
+            @Parameter(description = ASSEMBLY_DESC)
+            @RequestParam(required = false) String assembly
+    ) {
+        List<String> processList = inputs;
+        if (inputs.size() > MAX_INPUT) {
+            processList = inputs.subList(0, MAX_INPUT);
+        }
+        List<UserInput> userInputs = InputProcessor.parse(processList);
+        InputParams params = InputParams.builder()
+                .inputs(userInputs)
+                .fun(function)
+                .pop(population)
+                .str(structure)
+                .assembly(assembly)
+                .summarise(true) // not needed anymore
+                .build();
+
+        MappingResponse mappingResponse = mappingFetcher.getMapping(params);
+        if (inputs.size() > MAX_INPUT && mappingResponse != null && mappingResponse.getMessages() != null) {
+            mappingResponse.getMessages().add(new Message(Message.MessageType.WARN, String.format("Processed first %d inputs only.", MAX_INPUT)));
+        }
+
+        // Post-fetch: add input summary to response
+        if (mappingResponse != null) {
+            String inputSummary = InputProcessor.summary(userInputs).toString();
+            mappingResponse.getMessages().add(new Message(Message.MessageType.INFO, inputSummary));
+        }
+
+        return new ResponseEntity<>(mappingResponse, HttpStatus.OK);
     }
 
-    InputParams params = InputParams.builder()
-            .inputs(InputProcessor.parse(processList))
-            .fun(function)
-            .pop(population)
-            .str(structure)
-            .assembly(assembly)
-            .summarise(true)
-            .build();
+    // TODO
+    // perhaps a single GET /mapping endpoint that takes an InputType param
+    // inputType=ID|PROTEIN|SINGLE_VARIANT
+    // input=id or accession or single (or could be list of) input/variants
+    // note: current single_query and /mapping/query endpoint seems to be for the same purpose (or direct query, or non-cached input)
+    // move below to PagedMappingResponse/isolate this endpoint
 
-    MappingResponse mappingResponse = mappingFetcher.getMapping(params);
-    if (inputs.size() > MAX_INPUT && mappingResponse != null && mappingResponse.getMessages() != null) {
-      mappingResponse.getMessages().add(new Message(Message.MessageType.WARN, String.format("Processed first %d inputs only.", MAX_INPUT)));
+    @Operation(
+            summary = "Retrieve Mapping for a Single Input Query",
+            description = "Fetch the genomic-protein mapping for a single input query provided as search parameter in the URL. " +
+                    "The input should be in a supported format, such as genomic coordinates, variant identifiers, or protein change notations. " +
+                    "Default input params: annotations false, assembly null, and summarise false."
+    )
+    @GetMapping(value = "/mapping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MappingResponse> mapping(
+            @Parameter(description = "The input query in a supported format.", example = "19-1010539-G-C")
+            @RequestParam String input) {
+        InputParams params = InputParams.builder()
+                .inputs(InputProcessor.parse(List.of(input)))
+                .build();
+        MappingResponse mappingResponse = mappingFetcher.getMapping(params);
+        return new ResponseEntity<>(mappingResponse, HttpStatus.OK);
     }
-    return new ResponseEntity<>(mappingResponse, HttpStatus.OK);
-  }
-
-  @Operation(
-          summary = "Retrieve Mapping for a Single Input Query",
-          description = "Fetch the genomic-protein mapping for a single input query provided as search parameter in the URL. " +
-                  "The input should be in a supported format, such as genomic coordinates, variant identifiers, or protein change notations. " +
-                  "Default input params: annotations false, assembly null, and summarise false."
-  )
-  @GetMapping(value = "/mapping", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<MappingResponse> mapping(
-          @Parameter(description = "The input query in a supported format.", example = "19-1010539-G-C")
-          @RequestParam String input) {
-      InputParams params = InputParams.builder()
-              .inputs(InputProcessor.parse(List.of(input)))
-              .build();
-    MappingResponse mappingResponse = mappingFetcher.getMapping(params);
-    return new ResponseEntity<>(mappingResponse, HttpStatus.OK);
-  }
 
 }
