0fc3447c3573445b1e7aa34791d57ce18c6d783e
prabhat <prabhat@ebi.ac.uk>
2025-04-09 12:18:13 +0100

    cleaned up constants

--- src/main/java/uk/ac/ebi/protvar/utils/Constants.java
+++ src/main/java/uk/ac/ebi/protvar/utils/Constants.java
@@ -6,4 +6,12 @@ public class Constants {
 	public static final String SPLITTER = "-";
 	public static final String COMMA = ",";
 	public static final String NA = "N/A";
+
+	// Proteins and Variation API parameters
+	public static final String PARAM_ACCESSION = "accession";
+	public static final String PARAM_TAXID = "taxid";
+	public static final String PARAM_LOCATION = "location";
+	public static final String TAX_ID_HUMAN = "9606";
+	public static final String URL_PATH_HGVS = "hgvs/";
+
 }
