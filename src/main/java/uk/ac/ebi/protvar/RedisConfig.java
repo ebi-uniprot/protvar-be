f3d410394138515fe9ff170a6c27ab773c8ab536
prabhat <prabhat@ebi.ac.uk>
2025-04-23 22:47:43 +0100

    redis config changes around serializer and cache mgr

--- src/main/java/uk/ac/ebi/protvar/RedisConfig.java
+++ src/main/java/uk/ac/ebi/protvar/RedisConfig.java
@@ -3,13 +3,17 @@ package uk.ac.ebi.protvar;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -21,17 +25,14 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration =
-                new RedisStandaloneConfiguration(redisHost, redisPort);
-        configuration.setPassword("");
-        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder()
-                .build();
-        return new JedisConnectionFactory(configuration, jedisClientConfiguration);
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
     }
 
+    // Create a serializer with custom ObjectMapper
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public GenericJackson2JsonRedisSerializer redisJsonSerializer() {
         // Create and configure ObjectMapper with polymorphic typing
         ObjectMapper mapper = new ObjectMapper();
         mapper.activateDefaultTyping(
@@ -40,18 +41,38 @@ public class RedisConfig {
                         .build(),
                 ObjectMapper.DefaultTyping.NON_FINAL
         );
+        return new GenericJackson2JsonRedisSerializer(mapper);
+    }
 
-        // Create a serializer with custom ObjectMapper
-        GenericJackson2JsonRedisSerializer serializer =
-                new GenericJackson2JsonRedisSerializer(mapper);
-
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory,
+                                                       GenericJackson2JsonRedisSerializer redisJsonSerializer) {
         // Configure RedisTemplate
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
-        template.setEnableTransactionSupport(true);
+        template.setValueSerializer(redisJsonSerializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(redisJsonSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    /**
+     * This ensures that Spring's @Cacheable mechanism will use Jackson (JSON) serialization, not the default JDK one.
+     * @param connectionFactory
+     * @param redisJsonSerializer
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory,
+                                     GenericJackson2JsonRedisSerializer redisJsonSerializer) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisJsonSerializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
