e6dde57ca9677ae2e3818cce11ba7a38a64f08ba
prabhat <prabhat@ebi.ac.uk>
2025-06-24 21:29:38 +0100

    register java time module for localdatetime to work

--- src/main/java/uk/ac/ebi/protvar/JacksonConfig.java
+++ src/main/java/uk/ac/ebi/protvar/JacksonConfig.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.protvar;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -22,6 +23,8 @@ public class JacksonConfig {
         SimpleModule module = new SimpleModule();
         module.addDeserializer(Comment.class, new CommentDeserializer());
         objectMapper.registerModule(module);
+
+        objectMapper.registerModule(new JavaTimeModule());
 
         return objectMapper;
     }
