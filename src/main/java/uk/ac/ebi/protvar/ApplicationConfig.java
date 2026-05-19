24d42f8813742d1ab94c4b9064d4977c9ef60764
prabhat <prabhat@ebi.ac.uk>
2026-05-19 13:32:38 +0100

    Remove dead code: external-API leftovers and unused Gatling load tests
    - Drop unused RestTemplateCache, retryRestTemplateCustomizer bean, and
      Proteins/Variation API constants left over from the old API-calling design
    - Remove the Gatling load-test setup (untouched since 2024, stale endpoints):
      source, fixtures, deps; plus TestHelper which only fed it
    - Rewrite README; document backing services and no-external-API status

--- src/main/java/uk/ac/ebi/protvar/ApplicationConfig.java
+++ src/main/java/uk/ac/ebi/protvar/ApplicationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -81,23 +80,6 @@ public class ApplicationConfig {
             }
         });
         return restTemplate;
-    }
-
-    @Bean
-    RestTemplateCustomizer retryRestTemplateCustomizer() {
-        return restTemplate -> restTemplate.getInterceptors().add((request, body, execution) -> {
-
-            RetryTemplate retryTemplate = new RetryTemplate();
-            retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3));
-            try {
-                return retryTemplate.execute(context -> {
-                    System.out.println("start retrying ....");
-                    return execution.execute(request, body);
-                });
-            } catch (Throwable throwable) {
-                throw new RuntimeException(throwable);
-            }
-        });
     }
 
 }
